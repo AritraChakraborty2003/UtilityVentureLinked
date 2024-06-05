c15bd0a17ef9401fd19af557430da3cd07451d12
AritraChakraborty2003 <aritra.chakraborty203@gmail.com>
2024-06-05 12:43:41 +0530

    update: updae in first page design

--- client/my-project/src/components/First.jsx
+++ client/my-project/src/components/First.jsx
@@ -10,7 +10,7 @@ const First = () => {
     font: "poppins",
   };
   const [text] = useTypewriter({
-    words: ["Businesses", "Entreprenership", "lives"],
+    words: ["Businesses", "startups", "lives"],
     loop: {},
     typeSpeed: 120,
     deleteSpeed: 90,
@@ -22,18 +22,18 @@ const First = () => {
         className="h-[calc(100vh-15vmin)] md:h-[calc(100vh-10vmin)] w-[100vw] flex justify-center items-center flex-wrap-reverse  p-4 overflow-hidden"
         style={myStyle}
       >
-        <div className="leftPart flex flex-col lg:justify-center lg:items-start  w-[83vmin] pb-3 lg:h-[100vmin]">
-          <p className="lg:mt-[8vmin] text-[12vmin] text-black pb-3 font-extrabold">
+        <div className="leftPart flex flex-col lg:justify-center lg:items-start  w-[83vmin] pb-3 lg:h-[100vmin] sm:mt-[-4vmin]">
+          <p className="lg:mt-[8vmin] text-[13.5vmin] lg:text-[12vmin] text-black pb-3 font-extrabold">
             Welcome To,
           </p>
 
-          <p className="text-[8.5vmin] text-black pb-3 font-medium mt-[-4vmin]">
+          <p className="text-[10vmin] lg:text-[8.5vmin] text-black pb-3 font-medium mt-[-4vmin]">
             Venture Linked
           </p>
-          <p className="text-[3.5vmin] text-black pb-3 font-medium mt-[-2vmin]">
+          <p className="text-[5vmin] lg:text-[3.5vmin] text-black pb-3 font-medium mt-[-2vmin]">
             Utility Software Solution
           </p>
-          <p className="text-[3vmin] lg:text-[2.75vmin] text-black pb-3 font-normal">
+          <p className="text-[3.75vmin] lg:text-[2.75vmin] text-black pb-3 font-normal">
             ( Connecting entrpreneurs evolving{" "}
             <span style={{ color: "black", fontWeight: "bold" }}> {text}</span>
             <span style={{ color: "black" }}>
@@ -52,7 +52,7 @@ const First = () => {
           </button>
         </div>
         <div className="rightPart w-[70vmin] lg:h-[100vmin] lg:p-10  flex justify-center items-center">
-          <div className="mt-[3vmin] lg:mt-[8vmin]  lg:ml-[4vmin]  imgHolder h-[70vmin] w-[70vmin] lg:w-[74vmin] lg:h-[55vmin] 2xl:mt-[10vmin]">
+          <div className="mt-[3vmin] lg:mt-[8vmin]  lg:ml-[4vmin]  imgHolder h-[65vmin] w-[90vw] lg:w-[74vmin] lg:h-[55vmin] 2xl:mt-[10vmin]">
             <img
               src="./startup.png"
               className="max-w-full max-h-full object-contain"
